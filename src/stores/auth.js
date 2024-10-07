3e568d1b30aefb4d4dc88e56fe21a80ca1549775
jin <phjin0701@gmail.com>
2024-10-07 10:44:21 +0900

    temp login auth.js

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,7 +1,41 @@
+// stores/user.js
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import axios from 'axios';
 
-export const useAuthStore = defineStore('auth', {
-	state: () => ({}),
-	getters: {},
-	actions: {},
+export const useUserStore = defineStore('user', () => {
+	const isLoggedIn = ref(false);
+	const userInfo = ref(null);
+
+	// 로그인 상태 설정 함수
+	function setLoginStatus(status) {
+		isLoggedIn.value = status;
+	}
+
+	// 유저 정보 설정 함수
+	function setUserInfo(info) {
+		userInfo.value = info;
+	}
+
+	// JSON Server에서 유저 정보 가져오기
+	async function fetchUserInfo() {
+		try {
+			const response = await axios.get('http://localhost:8089/members');
+			if (response.data) {
+				setUserInfo(response.data);
+				setLoginStatus(true);
+				console.log('로그인 정보: ', userInfo);
+			}
+		} catch (error) {
+			console.error('Failed to fetch user info:', error);
+		}
+	}
+
+	return {
+		isLoggedIn,
+		userInfo,
+		setLoginStatus,
+		setUserInfo,
+		fetchUserInfo,
+	};
 });
