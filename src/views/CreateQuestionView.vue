d0157a8b5f7988cadec7ec4593576ec2395d4a44
jin <phjin0701@gmail.com>
2024-10-10 02:33:54 +0900

    Feat Create Question Page

--- src/views/CreateQuestionView.vue
+++ src/views/CreateQuestionView.vue
@@ -3,12 +3,22 @@
 		<SelectCategoryComp></SelectCategoryComp>
 		<input type="text" class="large-input" placeholder="제목을 입력하세요" />
 		<SelectHashTagComp></SelectHashTagComp>
+		<TextEditorComp class="text-editor"></TextEditorComp>
+		<div class="button-container">
+			<router-link to="/question">
+				<button class="cancel-button">취소</button>
+			</router-link>
+			<router-link to="/question">
+				<button class="save-button">저장</button>
+			</router-link>
+		</div>
 	</div>
 </template>
 
 <script setup>
 import SelectCategoryComp from '@/components/SelectCategoryComp.vue';
 import SelectHashTagComp from '@/components/SelectHashTagComp.vue';
+import TextEditorComp from '@/components/TextEditorComp.vue';
 </script>
 
 <style scoped>
@@ -36,5 +46,36 @@ import SelectHashTagComp from '@/components/SelectHashTagComp.vue';
 
 .large-input::placeholder {
 	font-size: 10vh; /* placeholder는 크지만 실제 입력 내용은 작게 조정 */
+}
+
+.text-editor {
+	margin-top: 50px;
+	width: 70%; /* TextEditorComp의 너비를 설정합니다 */
+	height: 600px;
+}
+
+.button-container {
+	width: 70%; /* 버튼 컨테이너의 너비를 TextEditorComp와 동일하게 설정합니다 */
+	display: flex;
+	justify-content: flex-end; /* 버튼들을 오른쪽으로 정렬 */
+	margin-top: 60px; /* 버튼 상단 여백 */
+}
+
+.cancel-button,
+.save-button {
+	padding: 0.5rem 1rem;
+	margin-left: 10px;
+	font-size: 1rem;
+	border: none;
+	cursor: pointer;
+	font-weight: bold;
+	border-radius: 10px;
+	margin-bottom: 100px;
+	width: 70px;
+}
+
+.save-button {
+	background-color: #369261;
+	color: white;
 }
 </style>
