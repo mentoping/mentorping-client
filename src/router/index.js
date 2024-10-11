612e337bbce8da20f078f859bee472652009d12e
jin <phjin0701@gmail.com>
2024-10-11 14:18:24 +0900

    Feat Mentoring View Routing

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import QuestionDetail from '@/views/QuestionDetail.vue';
 import ChatRoom from '@/views/ChatRoom.vue';
 import InquiryView from '@/views/InquiryView.vue';
 import CreateQuestionView from '@/views/CreateQuestionView.vue';
+import CreateMentoringView from '@/views/CreateMentoringView.vue';
 
 const routes = [
 	{
@@ -38,6 +39,7 @@ const routes = [
 	},
 	{
 		path: '/login',
+		name: 'Login',
 		component: LoginView,
 	},
 	{
@@ -54,6 +56,11 @@ const routes = [
 		path: '/create-question',
 		name: 'CreateQuestion',
 		component: CreateQuestionView,
+	},
+	{
+		path: '/create-mentoring',
+		name: 'CreateMentoring',
+		component: CreateMentoringView,
 	},
 ];
 
