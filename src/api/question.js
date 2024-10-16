e58f7f89154c3a7223bb1438cf4b8ce7ba808397
jin <phjin0701@gmail.com>
2024-10-17 04:23:40 +0900

    Fix Detail Question Comp

--- src/api/question.js
+++ src/api/question.js
@@ -68,11 +68,9 @@ export const questionPagingSearch = async (category, sort, page, keyword) => {
 //디테일 페이지에서 현재 페이지의 질문 정보를 가져오는(질문만)
 export const fetchDetailQuestion = async questionId => {
 	try {
-		const response = await axios.get(
-			// `http://localhost:3000/question-detail/${questionId}`,
-			'http://localhost:8089/question-detail',
-		);
-		console.log(questionId); //에러 때문에 넣어본거. 잘 넘어오는지 확인
+		const response = await axios.get(`/api/questions/${questionId}`, {
+			withCredentials: true,
+		});
 		return response.data;
 	} catch (error) {
 		console.error('Error fetching question detail:', error);
